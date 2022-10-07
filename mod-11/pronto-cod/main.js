6f133eedbd337dd47c6820e8e8957f9840b40d91
Eduardo Tavares <eduardo91423344@gmail.com>
2022-10-07 20:30:00 -0300

    será que agora dá certo?!

---
+++ mod-11/pronto-cod/main.js
@@ -0,0 +1,28 @@
+let btnM = document.querySelector("#btn-M");
+let ulLincks = document.querySelector(".lincks");
+btnM.onclick = () => {
+    btnM.classList.toggle('fa-times');
+    ulLincks.classList.toggle('active');
+}
+
+// meu primeiro objeto criado só de exemplo
+// const book = {
+//     "title": " hello word",
+    
+
+//     form: {
+//         name: "edu",
+//         idade: "23"
+//     }
+
+// };
+// const mand = {
+//     name: "eduardo",
+//     idade: 22,
+//     cidade: "horizonte",
+//     emprego: "Dev front-end"
+
+// };
+
+
+    // fim objeto
